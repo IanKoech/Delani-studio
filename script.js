5d0c06da87d73d7df37b92393481d8aad7b05313
Ian K <iankoech9@gmail.com>
2020-07-27 08:42:10 +0300

    Added toggle effect on portfolio image in js file

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
-    $("div#design").click(function(){
-        $(".image1").hide();
+    $("#image1").click(function(){
+        $("#image1").hide();
+        $(".design-text").toggle();
+        $(".design-text").show();
+    });
+    $(".design-text").click(function(){
+        $(".design-text").hide();
+        $("#image1").toggle();
+        $("#image1").show();
+    });
+});
+$(document).ready(function(){
+    $("#image2").click(function(){
+        $("#image2").hide();
+        $(".development-text").toggle();
+        $(".development-text").show();
+    });
+    $(".development-text").click(function(){
+        $(".development-text").hide();
+        $("#image2").toggle();
+        $("#image2").show();
+    });
+});
+$(document).ready(function(){
+    $("#image3").click(function(){
+        $("#image3").hide();
+        $(".management-text").toggle();
+        $(".management-text").show();
+    });
+    $(".management-text").click(function(){
+        $(".management-text").hide();
+        $("#image3").toggle();
+        $("#image3").show();
     });
 });
